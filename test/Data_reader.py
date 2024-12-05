0384a3de1bf32fd6610066a3a8768a8f98fa95dc
Oleg aka Waxmater <01eg.4ursanov@gmail.com>
2024-12-05 15:44:52 +0300

    create Data_reader class

---
+++ test/Data_reader.py
@@ -0,0 +1,19 @@
+class Data_reader:
+    """Класс для получения данных"""
+    def read_lines_from_file(self, file_path: str) -> list[str]:
+        """
+        Читает данные из файла и возвращает список строк.
+
+        :param file_path: Путь к файлу
+        :return: Список строк из файла
+        """
+        try:
+            with open(file_path, 'r') as file:
+                lines = file.readlines()  # Чтение всех строк из файла
+                return [line.strip() for line in lines]  # Удаление символов новой строки и лишних пробелов
+        except FileNotFoundError:
+            print(f"Файл '{file_path}' не найден.")
+            return []
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+            return []
